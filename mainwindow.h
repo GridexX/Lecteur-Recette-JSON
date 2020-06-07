05c921582cc532fcae2103ed0de0aead9d2a069a
FOUGEROUSE Arsene <arsene.fougerouse@etu.univ-amu.fr>
2020-06-07 18:46:12 +0200

    Documentation de la classe Recette + modif de la MainWindow

--- mainwindow.h
+++ mainwindow.h
@@ -21,7 +21,7 @@ class MainWindow : public QMainWindow
 
 public:
     /**
-      * @fn MainWindow(QWidget *parent = nullptr)
+      * @fn MainWindow(QWidget *parent)
       * @brief constructeur de la fonction MainWindow
       * @param parent : précise si le widget hérite d'une autre fenêtre
       */
@@ -35,22 +35,22 @@ public:
 
 private slots:
     /**
-     * @fn first_state
+     * @fn first_state()
      * @brief fonction liée à la QStateMachine. Permet de maquer le bouton précédent en arrivant sur la première étape
      */
     void first_state();
     /**
-     * @fn last_state
+     * @fn last_state()
      * @brief fonction liée à la QStateMachine. Permet de maquer le bouton suivant en arrivant sur la dernière étape
      */
     void last_state();
     /**
-     * @fn first_state_exit
+     * @fn first_state_exit()
      * @brief fonction liée à la QStateMachine. Permet de ré-afficher le bouton précédent en sortie de la première étape
      */
     void first_state_exit();
     /**
-     * @fn last_state_exit
+     * @fn last_state_exit()
      * @brief fonction liée à la QStateMachine. Permet de ré-affichant le bouton suivant en sortie de la dernière étape
      */
     void last_state_exit();
@@ -63,42 +63,43 @@ private:
 
 public slots :
     //slots pour modifs les labels
-    /* @fn modifNom
+    /* @fn modifNom(QString)
      * @brief slot qui modifie le nom de la recette
      * @param le nom sous forme de chaine de caractère
      */
     void modifNom(QString);
 
-    /* @fn modifDesc
+    /* @fn modifDesc(QString)
      * @brief slot qui modifie la description
      * @param la description sous forme de chaine de caractère
      */
     void modifDesc(QString);
 
-    /* @fn modifMotsCles
+    /* @fn modifMotsCles(QString)
      * @brief slot qui modifie les mots cles
      * @param les mots cles sous forme de chaine de caractère
      */
     void modifMotsCles(QString);
 
-    /* @fn modifIng
+    /* @fn modifIng(QStringList)
      * @brief slot qui modifie la liste des ingrédients
      * @param les ingrédients sous forme de liste de chaine de caractere
      */
     void modifIng(QStringList);
 
-    /* @fn modifEtapes
+    /* @fn modifEtapes(QStringList)
      * @brief slot qui modifie le la liste des etapes
      * @param les étapes sous formes de liste de chaine de caractere
      */
     void modifEtapes(QStringList);
 
-    /* @fn modifTemps
+    /* @fn modifTemps(QStringList)
      * @brief slot qui modifie le temps nécessaire pour préparer la recette
      * @param les différents temps sous forme de liste de chaine de caractère
      */
     void modifTemps(QStringList);
-    /* @fn modifURL
+
+    /* @fn modifURL(QString)
      * @brief slot qui modifie l'url pour accéder à la recette
      * @param l'url sous forme de chaine de caractère
      */
--- recette.h
+++ recette.h
@@ -13,44 +13,140 @@ class Recette : public QObject
 {
     Q_OBJECT
 public:
+    /* @fn Recette(QObject *parent)
+     * @brief constructeur de la classe Recette
+     * @param parent : précise si le widget hérite d'une autre fenêtre
+     */
     explicit Recette(QObject *parent = nullptr);
+
+    /* @fn getNom()
+     * @brief fonction getter pour obtenir le nom de la recette
+     */
     QString getNom() const;
+
+    /* @fn setNom(const QStringList &)
+     * @brief fonction setter pour modifier le nom de la recette
+     * @param value : le nom de la recette de cuisine
+     */
     void setNom(const QString &value);
 
+    /* @fn getDescription()
+     * @brief fonction getter pour obtenir la description de la recette
+     */
     QString getDescription() const;
+
+    /* @fn setDescription(const QStringList &)
+     * @brief fonction setter pour modifier le nom de la recette
+     * @param value : la description de la recette de cuisine
+     */
     void setDescription(const QString &value);
 
+    /* @fn getListeIngredients()
+     * @brief fonction getter pour obtenir la liste des ingrédient de la recette
+     */
     QStringList getListeIngredients() const;
+
+    /* @fn setListeIngredients(const QStringList &)
+     * @brief fonction setter pour modifier le nom de la recette
+     * @param value : la liste d'ingrédient de la recette de cuisine
+     */
     void setListeIngredients(const QStringList &value);
 
+
+    /* @fn getListeEtapes()
+     * @brief fonction getter pour obtenir la liste des étapes de la recette
+     */
     QStringList getListeEtapes() const;
+
+    /* @fn setListeEtapes(const QStringList &)
+     * @brief fonction setter pour modifier la liste des étapes de la recette
+     * @param value : la liste d'étapes de la recette de cuisine
+     */
     void setListeEtapes(const QStringList &value);
 
+    /* @fn getTemps()
+     * @brief fonction getter pour obtenir les différents temps de la recette
+     */
     QStringList getTemps() const;
+
+    /* @fn setTemps(const QStringList &)
+     * @brief fonction setter pour modifier les différents temps de la recette
+     * @param value : les différents temps de la recette de cuisine
+     */
     void setTemps(const QStringList &value);
 
+    /* @fn getURL()
+     * @brief fonction getter pour obtenir l'url de la recette
+     */
     QString getURL() const;
+
+    /* @fn setURL(const QString &)
+     * @brief fonction setter pour modifier les différents temps de la recette
+     * @param value : l'url de la recette de cuisine
+     */
     void setURL(const QString &value);
 
+    /* @fn getMotsCles()
+     * @brief fonction getter pour obtenir les mots clés de la recette
+     */
     QString getMotsCles() const;
+
+    /* @fn setMotsCles(const QString &)
+     * @brief fonction setter pour modifier les mots clés de la recette
+     * @param value : les mots clés de la recette de cuisine
+     */
     void setMotsCles(const QString &value);
 
 private:
-    QString nom;
-    QString description;
-    QString motsCles;
-    QStringList listeIngredients;
-    QStringList listeEtapes;
-    QStringList temps;
-    QString URL;
+    QString nom; /*!< nom: Le nom de la recette */
+    QString description; /*!< description: La description de la recette */
+    QString motsCles; /*!< motsCles: Les mots-clefs  de la recette */
+    QStringList listeIngredients; /*!< listeIngredients: La liste des ingrédients de la recette */
+    QStringList listeEtapes; /*!< listeEtapes: La liste des étapes de la recette */
+    QStringList temps; /*!< temps: les différents temps pour réaliser la recette */
+    QString URL; /*!< URL: L'url pour accéder à la page web de la recette */
 
 signals:
+    /* @sa envoieNom(QString)
+     * @brief fonction pour envoyer le nom à la classe MainWindow (voir classe MainWindow)
+     * @param nom : le nom à transmettre
+     */
     void envoieNom(QString);
+
+    /* @sa envoieDesc(QString)
+     * @brief fonction pour envoyer la description à la classe MainWindow (voir classe MainWindow)
+     * @param desc : la description à transmettre
+     */
     void envoieDesc(QString);
+
+    /* @sa envoieMotsCles(QString)
+     * @brief fonction pour envoyer les mots cles à la classe MainWindow (voir classe MainWindow)
+     * @param MotsCles : les mots cles à transmettre
+     */
     void envoieMotsCles(QString);
+
+    /* @sa envoieIng(QStringList)
+     * @brief fonction pour envoyer la liste des ingrédients à la classe MainWindow (voir classe MainWindow)
+     * @param ingredients : la liste des ingrédients à transmettre
+     */
     void envoieIng(QStringList);
+
+    /* @sa envoieEtapes(QStringList)
+     * @brief fonction pour envoyer la liste des étapes à la classe MainWindow (voir classe MainWindow)
+     * @param étapes : la liste des étapes à transmettre
+     */
     void envoieEtapes(QStringList);
+
+    /* @sa envoieTemps(QStringList)
+     * @brief fonction pour envoyer les temps à la classe MainWindow (voir classe MainWindow)
+     * @param temps : la liste des temps à transmettre
+     */
     void envoieTemps(QStringList);
+
+    /* @sa envoieURL(QStringList)
+     * @brief fonction pour envoyer l'url à la classe MainWindow (voir classe MainWindow)
+     * @param url : l'url du site web de la recette à transmettre
+     */
     void envoieURL(QString);
 };
 
