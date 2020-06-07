5912f33870b9de0aab70cdd84284e1f54dc9d259
POLLET Lucas <lucas.pollet@etu.univ-amu.fr>
2020-06-07 18:59:07 +0200

    Documentation traitement.h

--- traitement.h
+++ traitement.h
@@ -16,14 +16,27 @@
 class traitement
 {
 public :
+    /**
+     * \fn traitement(QJsonDocument)
+     * \brief Constructeur du traitemment json
+     * \param QJsonDocument : chemin d'accès du fichier json de la recette
+     */
     traitement(QJsonDocument);
+    /**
+     * \fn setRecette()
+     * \brief Fonction qui permet de lire le fichier et enregistrer la recette
+     */
     void setRecette();
+    /**
+     * \fn connection()
+     * \brief Fonction qui permet d'initialiser l'instance et la connexion
+     */
     void connection();
 
 private :
-    QJsonDocument docJson;
-    Recette *recette;
-    MainWindow *w;
+    QJsonDocument docJson; /*!< docJson: le chemin d'accès du fichier json */
+    Recette *recette; /*!< Recette *recette: pointeur sur la variable recette */
+    MainWindow *w;  /*!< Mainwindow *w: pointeur sur la fenetre principale */
 
 };
 
